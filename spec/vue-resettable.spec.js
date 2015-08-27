ab6e561768109fd287e402de14f418f0ada117a8
shotaro.akiyama <satetsu888@gmail.com>
2015-08-27 23:34:26 +0900

    Add simple test

---
+++ spec/vue-resettable.spec.js
@@ -0,0 +1,6 @@
+describe('vue-resettable', function () {  
+    it('should have a created hook', function () {
+        expect(typeof resettable.created).toBe('function')
+        console.log("test running");
+    })
+})
