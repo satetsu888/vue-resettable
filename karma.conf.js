88377ffeccd355eec502215bb34982bb1dfaa660
shotaro.akiyama <satetsu888@gmail.com>
2015-08-28 08:26:27 +0900

    fix test command

--- karma.conf.js
+++ karma.conf.js
@@ -77,7 +77,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // --no-sandbox Chrome for Travis CI
     customLaunchers: {
