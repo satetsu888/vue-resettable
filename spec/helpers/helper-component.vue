651b1da42b91bec68a92df34a047fbba6f0c55a0
shotaro.akiyama <satetsu888@gmail.com>
2015-08-28 23:05:48 +0900

    Add test

---
+++ spec/helpers/helper-component.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>name: <input type=text v-model="name"> changed: {{changed("name")}}</p>
+        <p>nickname: <input type=text v-model="nickname"> changed: {{changed("nickname")}}</p>
+        <p>age: <input type=range min=0 max=200 v-model="age">{{age}} change: {{changed("age")}}</p>
+
+        changed: {{changed()}} <br>
+        <input type=button value="Reset" v-on="click: reset">
+        <input type=button value="Update" v-on="click: update()">
+    </div>
+</template>
+
+<script>
+var resettable = require('../../index.js');
+var Vue = require('vue');
+module.exports = Vue.extend({
+    components: {
+        'helper-component': require('./helper-component.vue')
+    },
+    mixins: [ resettable ]
+});
+</script>
--- spec/vue-resettable.spec.js
+++ spec/vue-resettable.spec.js
@@ -1,7 +1,24 @@
-var resettable = require('../index.js');
-describe('vue-resettable', function () {  
+describe('require vue-resettable', function () {  
+    var resettable = require('../index.js');
+
     it('should have a created hook', function () {
         expect(typeof resettable.created).toBe('function')
-        console.log("test running");
     })
 })
+
+describe('mixin vue-resettable', function () {  
+    var helper = require('./helpers/helper-component.vue');
+    var vm = new helper({
+        name    : "ihr",
+        nickname: "rbbish-bin",
+        age     : 28
+    });
+
+    it('should have expected function', function () {
+        expect(typeof vm.reset).toBe('function')
+        expect(typeof vm.update).toBe('function')
+        expect(typeof vm.getBase).toBe('function')
+        expect(typeof vm.changed).toBe('function')
+    })
+})
+
