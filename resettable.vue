9c580ca00a3598980a0c9b3a97a9762a026b5161
shotaro.akiyama <satetsu888@gmail.com>
2015-08-29 15:55:22 +0900

    Rename base and current variables to _base, _current

--- resettable.vue
+++ resettable.vue
@@ -2,44 +2,44 @@
 module.exports = {
     data: function(){
         return { 
-            base: {},
-            current: {},
+            _base: {},
+            _current: {},
         }
     },
     created: function(){
         var initialData = JSON.parse(JSON.stringify(this._data));
         
-        delete initialData["base"];
-        delete initialData["current"];
+        delete initialData["_base"];
+        delete initialData["_current"];
         this.update(initialData);
     },
     methods: {
         update: function(newBaseData){
             var self = this;
-            var baseData = newBaseData ? newBaseData : self.current;
+            var baseData = newBaseData ? newBaseData : self._current;
 
             Object.keys(baseData).forEach(function(key){
-                self.$watch(key, function(newVal, oldVal){ self.current[key] = newVal },{ deep: true});
+                self.$watch(key, function(newVal, oldVal){ self._current[key] = newVal },{ deep: true});
             });
-            this.base = JSON.parse(JSON.stringify(baseData));
+            this._base = JSON.parse(JSON.stringify(baseData));
 
             self.reset();
         },
         getBase: function(paramName){
-            return paramName ? this.base[paramName] : this.base;
+            return paramName ? this._base[paramName] : this._base;
         },
         reset: function(){
             var self = this;
-            self.current = JSON.parse(JSON.stringify(self.base));
-            Object.keys(self.base).forEach(function(key){
-                self.$data[key] = self.base[key];
+            self._current = JSON.parse(JSON.stringify(self._base));
+            Object.keys(self._base).forEach(function(key){
+                self.$data[key] = self._base[key];
             });
         },
         changed: function(paramName){
             if(paramName){
-                return JSON.stringify(this.base[paramName]) !== JSON.stringify(this.current[paramName]) ? true : false;
+                return JSON.stringify(this._base[paramName]) !== JSON.stringify(this._current[paramName]) ? true : false;
             } else {
-                return JSON.stringify(this.base) !== JSON.stringify(this.current) ? true : false;
+                return JSON.stringify(this._base) !== JSON.stringify(this._current) ? true : false;
             }
         }
     },
